d799e2b56c6b9bb3079b8d4a7ac20346bc6213ca
demel42 <demel42@web.de>
2019-03-02 15:29:34 +0000

    Apply fixes from StyleCI

--- scripts/37560.ips.php
+++ scripts/37560.ips.php
@@ -1,4 +1,5 @@
 <?php
+
 // -----------------------------------------------------------------------------
 // WebFront-freundliche HTML-Ausgabe aller defekten Instanzen, Skripte und Links
 // -----------------------------------------------------------------------------
@@ -7,100 +8,85 @@
 $ContentVariableID = 48011;
 // -----------------------------------------------------------------------------
 
-$content = '<body bgcolor="#0f2d4b">';  
+$content = '<body bgcolor="#0f2d4b">';
 
-$instanceStatusCodes = array(
+$instanceStatusCodes = [
     100 => 'module base status',
     101 => 'module is being created',
     102 => 'module created and running',
     103 => 'module is being deleted',
     104 => 'module is not beeing used'
-);
+];
 
 $errorCount = 0;
 
 $ids = IPS_GetInstanceList();
-foreach ($ids as $id)
-{
+foreach ($ids as $id) {
     $instance = IPS_GetInstance($id);
-    if ($instance['InstanceStatus'] > 103)
-    {
-        if ($errorCount == 0)
-        {
-            $content .= '<b>Defekte Instanzen:</b><br />'."\r\n";
+    if ($instance['InstanceStatus'] > 103) {
+        if ($errorCount == 0) {
+            $content .= '<b>Defekte Instanzen:</b><br />' . "\r\n";
         }
         $errorCount++;
-		$instanceStatus = $instance['InstanceStatus'];
-		if (isset($instanceStatusCodes[$instanceStatus])) {
-			$s = $instanceStatusCodes[$instanceStatus];
-		} else {
-			$s = 'unknown status ' . $instanceStatus;
-		}
-        $content .= '<span style="color: '.($instanceStatus >= 200 ? 'red' : 'grey').';">#'.$id.': '.IPS_GetLocation($id).': '.$s.'</span><br />'."\r\n";
+        $instanceStatus = $instance['InstanceStatus'];
+        if (isset($instanceStatusCodes[$instanceStatus])) {
+            $s = $instanceStatusCodes[$instanceStatus];
+        } else {
+            $s = 'unknown status ' . $instanceStatus;
+        }
+        $content .= '<span style="color: ' . ($instanceStatus >= 200 ? 'red' : 'grey') . ';">#' . $id . ': ' . IPS_GetLocation($id) . ': ' . $s . '</span><br />' . "\r\n";
     }
 }
 
-if ($errorCount > 0)
-{
-    $content .= '<br />'."\r\n";
+if ($errorCount > 0) {
+    $content .= '<br />' . "\r\n";
     $errorCount = 0;
 }
 
 $ids = IPS_GetScriptList();
 
-foreach ($ids as $id)
-{
+foreach ($ids as $id) {
     $script = IPS_GetScript($id);
-	
-    if ($script['ScriptIsBroken'])
-    {
-        if ($errorCount == 0)
-        {
-            $content .= '<b>Defekte Skripte:</b><br />'."\r\n";
+
+    if ($script['ScriptIsBroken']) {
+        if ($errorCount == 0) {
+            $content .= '<b>Defekte Skripte:</b><br />' . "\r\n";
         }
         $errorCount++;
-        $content .= '<span style="color: red;">#'.$id.': '.IPS_GetLocation($id).'</span><br />'."\r\n";
+        $content .= '<span style="color: red;">#' . $id . ': ' . IPS_GetLocation($id) . '</span><br />' . "\r\n";
     }
 }
 
-if ($errorCount > 0)
-{
-    $content .= '<br />'."\r\n";
+if ($errorCount > 0) {
+    $content .= '<br />' . "\r\n";
     $errorCount = 0;
 }
 
 $ids = IPS_GetLinkList();
 
-foreach ($ids as $id)
-{
+foreach ($ids as $id) {
     $link = IPS_GetLink($id);
-	
-    if (!IPS_ObjectExists($link['LinkID']))
-    {
-        if ($errorCount == 0)
-        {
-            $content .= '<b>Defekte Links:</b><br />'."\r\n";
+
+    if (!IPS_ObjectExists($link['LinkID'])) {
+        if ($errorCount == 0) {
+            $content .= '<b>Defekte Links:</b><br />' . "\r\n";
         }
         $errorCount++;
-        $content .= '<span style="color: red;">#'.$id.': '.IPS_GetLocation($id).'</span><br />'."\r\n";
+        $content .= '<span style="color: red;">#' . $id . ': ' . IPS_GetLocation($id) . '</span><br />' . "\r\n";
     }
 }
 
 $printContent = true;
 
-if (IPS_VariableExists((int)$ContentVariableID))
-{
+if (IPS_VariableExists((int) $ContentVariableID)) {
     $variable = IPS_GetVariable($ContentVariableID);
-	
-    if ($variable['VariableType'] === 3)
-    {
+
+    if ($variable['VariableType'] === 3) {
         $printContent = false;
         SetValueString($ContentVariableID, $content);
     }
 }
 
-if ($printContent)
-{
+if ($printContent) {
     echo $content;
 }
-?>
